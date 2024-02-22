7e166f310e06c6471beb779a96ee8bdbe80976e0
insertvalue <insertvalue@naver.com>
2024-02-22 17:43:29 +0900

    17.계좌목록만들기 1단계

--- src/main/java/com/tenco/bank/repository/interfaces/AccountRepository.java
+++ src/main/java/com/tenco/bank/repository/interfaces/AccountRepository.java
@@ -2,6 +2,8 @@ package com.tenco.bank.repository.interfaces;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 import com.tenco.bank.repository.model.Account;
 
 @Mapper  
@@ -12,8 +14,11 @@ public interface AccountRepository {
 	public int updateById(Account account);
 	public int deleteById(Integer id);
 	
-	// 계좌 조회 - 1 유저 , N 계좌 
-	public List<Account> findAllByUserId();
+	// 계좌 조회 - 1 유저 , N 계좌
+	// interface 파라미터명과 xml에 사용할 변수명을 다르게 해야 된다면 @Param 
+	// 어노테이션을 활용할 수 있습니다. 
+	// 2개 이상에 파라미터을 설계한다면 반드시 @Param 어노테이션을 지정해 주세요 
+	public List<Account> findAllByUserId(@Param("userId") Integer principalId);
 	public Account findByNumber(Integer id);
 	
 }
--- src/main/java/com/tenco/bank/service/AccountService.java
+++ src/main/java/com/tenco/bank/service/AccountService.java
@@ -1,5 +1,7 @@
 package com.tenco.bank.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import com.tenco.bank.dto.AccountSaveDTO;
 import com.tenco.bank.handler.exception.DataDeliveryException;
 import com.tenco.bank.handler.exception.RedirectException;
 import com.tenco.bank.repository.interfaces.AccountRepository;
+import com.tenco.bank.repository.model.Account;
 
 @Service
 public class AccountService {
@@ -38,6 +41,25 @@ public class AccountService {
 			// 예외 처리 - 에러 페이지로 이동
 			throw new RedirectException("알 수 없는 오류", HttpStatus.SERVICE_UNAVAILABLE);
 		}
+	}
+	
+	/**
+	 * 복잡한 Select 쿼리문일 경우 트랜잭션 처리를 해주 것이 좋습니다.  
+	 * 여기서는 단순한 Select 구문이라 바로 진행 합니다. 
+	 * @param principalId
+	 * @return
+	 */
+	public List<Account> readAccountListByUserId(Integer principalId) {
+		List<Account> accountListEntity = null;
+		try {
+			accountListEntity = accountRepository.findAllByUserId(principalId);
+		} catch (DataAccessException e) {
+			throw new DataDeliveryException("잘못된 처리 입니다", HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (Exception e) {
+			// 예외 처리 - 에러 페이지로 이동
+			throw new RedirectException("알 수 없는 오류", HttpStatus.SERVICE_UNAVAILABLE);
+		}
+		return accountListEntity;
 	}
 
 }
